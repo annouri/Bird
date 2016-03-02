ee1637e196bbadbd7f0136e65fbcb4c9b284d07b
annouri ilias <annouri.ilias@gmail.com>
2016-03-02 10:32:19 +0100

    CI Configuration Done

---
+++ application/config/email.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|*/
+
+$config['protocol'] = "smtp";
+$config['smtp_host'] = "ssl://smtp.googlemail.com";
+$config['smtp_port'] = "465";
+$config['smtp_user'] = "gerant.youpel@gmail.com"; 
+$config['smtp_pass'] = "youpelcompany";
+$config['charset'] = "utf-8";
+$config['wordwrap'] = TRUE;
+$config['mailtype'] = "html";
+$config['newline'] = "\r\n";
+
+?>
